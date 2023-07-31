506b814b232a04102781d124a9afab25df1e78a2
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-07-31 12:26:18 +0000

    i solved labe33

---
+++ drf_auth/snacks/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    
+    message ="you are not the owner"
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user or request.user.is_superuser
+        # return obj.owner == request.user  or request.user.is_staff
+
+    
